afa418c591eb4ad535aeafaae5d7a54694f51074
楠見　朋也 <kusutomo143@gmail.com>
2018-08-17 19:28:56 +0900

    initial commit

---
+++ app/Http/Controllers/TweetsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Tweet;
+
+class TweetsController extends Controller
+{
+  public function index()
+  {
+    $tweets = Tweet::all();
+    return view('tweets.index')->with('tweets', $tweets);
+  }
+}
