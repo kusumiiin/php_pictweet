4c65bee58818e6527d04a82d31ceb15dd7d8b66f
楠見　朋也 <kusutomo143@gmail.com>
2018-08-20 18:40:35 +0900

    delete nickname

--- app/Http/Controllers/TweetsController.php
+++ app/Http/Controllers/TweetsController.php
@@ -27,7 +27,6 @@ class TweetsController extends Controller
   {
     Tweet::create(
       array(
-        'name' => $request->name,
         'image' => $request->image,
         'text' => $request->text,
         'user_id' => Auth::user()->id
