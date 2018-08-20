9580ca19e59d6e7d2d02afb646722f05294854ad
楠見　朋也 <kusutomo143@gmail.com>
2018-08-20 18:37:57 +0900

    to solve n+1

--- app/Http/Controllers/TweetsController.php
+++ app/Http/Controllers/TweetsController.php
@@ -16,7 +16,7 @@ class TweetsController extends Controller
   }
   public function index()
   {
-    $tweets = Tweet::orderBy('created_at', 'DESC')->paginate(5);
+    $tweets = Tweet::with('user')->orderBy('created_at', 'DESC')->paginate(5);
     return view('tweets.index')->with('tweets', $tweets);
   }
   public function create()
