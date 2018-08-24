40ed33cedc2079a6584de693d2312d06198ab70c
楠見　朋也 <kusutomo143@gmail.com>
2018-08-24 16:22:13 +0900

    fix

--- app/Http/Controllers/TweetsController.php
+++ app/Http/Controllers/TweetsController.php
@@ -59,7 +59,7 @@ class TweetsController extends Controller
   }
   public function show($id)
   {
-    $tweet = Tweet::find($id)
+    $tweet = Tweet::find($id);
     return view('tweets.show')->with('tweet', $tweet);
   }
 
